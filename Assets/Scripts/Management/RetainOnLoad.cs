2b485c1e25a7e3b787dec32a811f66fd4321a1b0
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-06-15 10:33:56 -0500

    Last minute changes

--- Assets/Scripts/Management/RetainOnLoad.cs
+++ Assets/Scripts/Management/RetainOnLoad.cs
@@ -46,7 +46,7 @@ public class RetainOnLoad : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightControl) && (SceneManager.GetActiveScene().name != "Menu" && SceneManager.GetActiveScene().name != "Login"))
+        if ((Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.F2) ) && (SceneManager.GetActiveScene().name != "Menu" && SceneManager.GetActiveScene().name != "Login"))
         {
             SceneManager.LoadScene("Menu");
         }
